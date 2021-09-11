f1cd03ea607a9537c305532a98a06526034c580b
Mario Flores <mflores@sanservices.hn>
2021-09-10 20:36:38 -0600

    Estrcuctura Correcta

--- Proy/Proy.cpp
+++ Proy/Proy.cpp
@@ -2,90 +2,57 @@
 #ifdef _MSC_VER
 #define CRT_SECURE_NO_WARNINGS
 #endif
-#include <bits-stdc++.h>
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 struct traceRecord
 {
-    int32_t microOpCount;
-    uint64_t instructionAddress;
-    int32_t sourceRegister1;
-    int32_t sourceRegister2;
-    int32_t destinationRegister;
-    char conditionRegister;
-    char TNnotBranch;
-    char loadStore;
-    int64_t immediate;
-    uint64_t addressForMemoryOp;
-    uint64_t fallthroughPC;
-    uint64_t targetAddressTakenBranch;
-    char macroOperation[12];
-    char microOperation[23];
+    string memAddress;
+    char operationType;
+    traceRecord(): memAddress(""), operationType('R') {
+
+    }
 };
 
-int main() {
-    traceRecord bufferRead = 
-    {0,
-    	0,
-    	0,
-    	0,
-    	0,
-    	' ',
-    	' ',
-    	' ',
-    	0,
-    	0,
-    	0,
-    	0,
-    	' ',
-    	' '
-    };
+traceRecord hydrateRecord(traceRecord traceLine, string line) {
+    vector<string> words{};
+    stringstream sstream(line);
+    string word;
+    while (std::getline(sstream, word, ' ')) {
+        word.erase(std::remove_if(word.begin(), word.end(), ispunct), word.end());
+        words.push_back(word);
+    }
+    traceLine.memAddress = words.at(0);
+    traceLine.operationType = words.at(1).at(0);
+    return traceLine;
+}
 
+int main() {
     std::fstream fh;
-    string filename;
+    std::string filename;
     cout << "Archivo a leer" << endl;
     cin >> filename;
-    fh.open("C:/Users/Skytech/OneDrive/Documents/Q4 2021/Siso 2/Proyecto/"+filename+".trace", std::fstream::in | std::fstream::binary);
+    fh.open("C:/Users/Marzo/OneDrive/Documents/Q4 2021/Siso 2/Proyecto/"+filename+".trace", std::fstream::in | std::fstream::binary);
     int count{0};
     if (fh.is_open())
     {
-        while (!fh.eof())
+        std::string line;
+        while (std::getline(fh, line))
         {
-            fh.read(reinterpret_cast<char*>(&bufferRead.microOpCount), sizeof(bufferRead.microOpCount));
-            fh.read(reinterpret_cast<char*>(&bufferRead.instructionAddress), sizeof(bufferRead.instructionAddress));
-            fh.read(reinterpret_cast<char*>(&bufferRead.sourceRegister1), sizeof(bufferRead.sourceRegister1));
-            fh.read(reinterpret_cast<char*>(&bufferRead.sourceRegister2), sizeof(bufferRead.sourceRegister2));
-            fh.read(reinterpret_cast<char*>(&bufferRead.destinationRegister), sizeof(bufferRead.destinationRegister));
-            fh.read(&bufferRead.TNnotBranch, sizeof(bufferRead.TNnotBranch));
-            fh.read(&bufferRead.loadStore, sizeof(bufferRead.loadStore));
-            fh.read(reinterpret_cast<char*>(&bufferRead.immediate), sizeof(bufferRead.immediate));
-            fh.read(reinterpret_cast<char*>(&bufferRead.addressForMemoryOp), sizeof(bufferRead.addressForMemoryOp));
-            fh.read(reinterpret_cast<char*>(&bufferRead.fallthroughPC), sizeof(bufferRead.fallthroughPC));
-            fh.read(reinterpret_cast<char*>(&bufferRead.targetAddressTakenBranch), sizeof(bufferRead.targetAddressTakenBranch));
-            fh.read(reinterpret_cast<char*>(&bufferRead.macroOperation), sizeof(bufferRead.macroOperation));
-            fh.read(reinterpret_cast<char*>(&bufferRead.microOperation), sizeof(bufferRead.microOperation));
-            cout << "=================================================================================================================================================================" << endl;
-            cout << "========================================================================="<<"RECORD NO." << count << "========================================================================" << endl;
-        	cout << "=================================================================================================================================================================" << endl;
-            cout <<"1.Uop: ["<< bufferRead.microOpCount<<']'
-        	<< "||2.PC:[" << bufferRead.instructionAddress << ']'
-        	<< "||3.Src1:[" <<bufferRead.sourceRegister1 << ']'
-        	<< "||4.Src2:[" <<bufferRead.sourceRegister2 << ']'
-        	<< "||5.Dest:["<<bufferRead.destinationRegister << ']'
-        	<< "||6.Flags: [" <<bufferRead.conditionRegister << ']'
-        	<< "||7.Branch: [" <<bufferRead.TNnotBranch << ']'
-        	<< "||8.Ld/St: [" <<bufferRead.loadStore << ']'
-        	<< "||9.Immediate: ["<<bufferRead.immediate << ']'
-        	<< "||10.Mem.Address: [" <<bufferRead.addressForMemoryOp << ']'
-        	<< "||11.Fallthrough PC: [" <<bufferRead.fallthroughPC << ']'
-        	<< "||12.Target PC: ["<<bufferRead.targetAddressTakenBranch << ']'
-        	<< "||13.Macro Opcode: [" <<bufferRead.macroOperation << ']'
-        	<< "||14.Micro Opcode: [" <<bufferRead.microOperation << ']'
-        	<<std::endl<<std::endl;
-        	cout << "=================================================================================================================================================================" << endl;
+            traceRecord traceLine;
+            traceLine = hydrateRecord(traceLine, line);
+            cout << "----------------------------------------------------" << endl;
+            cout <<"Record ["<<count<<"]"<<" Mem.Address: [" << traceLine.memAddress << "]"<<"Operation Type: [" << traceLine.operationType<<"]" << endl;
             count++;
         }
     }
