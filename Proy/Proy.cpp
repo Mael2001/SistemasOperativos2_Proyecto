d84d7d6171060bb96210683cfbca51032261f026
Mael2001 <Mariof2001@unitec.edu>
2021-09-10 22:34:21 -0600

    LinkedListImplemented

--- Proy/Proy.cpp
+++ Proy/Proy.cpp
@@ -9,7 +9,6 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <iterator>
 #include <vector>
 #include <algorithm>
 
@@ -19,8 +18,38 @@ struct traceRecord
 {
     string memAddress;
     char operationType;
-    traceRecord(): memAddress(""), operationType('R') {
+    traceRecord(): memAddress(""), operationType(' ') {}
+};
+struct Node {
+    traceRecord data;
+    struct Node* next;
+    Node(traceRecord newData)
+    {
+        data = newData;
+        next = new Node();
+    }
+    Node(){}
+    inline string toString(int count)
+    {
+        string string =" Mem.Address: [";
+        string += data.memAddress;
+        string += "]";
+        string += "\t\tOperation Type: [";
+        string += data.operationType;
+        string += "]";
+       return  string;
+    }
+};
 
+struct NodeList
+{
+    Node* origin;
+    Node* lastNode;
+    inline string toString(int count)
+    {
+        string toString = "[Origin Node {" + origin->toString(count) + "}]";
+        toString+= "\n[Last Node {" + lastNode->toString(count) + "}]";
+        return toString;
     }
 };
 
@@ -36,23 +65,57 @@ traceRecord hydrateRecord(traceRecord traceLine, string line) {
     traceLine.operationType = words.at(1).at(0);
     return traceLine;
 }
-
 int main() {
+    /*
+     07b243a0 R
+	00228d40 R
+	00228d60 R
+	07b243c0 R
+	00113360 R
+	001fe308 R
+	00113380 R
+	001fe2f0 R
+	001133a0 R
+	001133c0 R
+	0022ccb0 R
+	001133e0 R
+	00113400 R  
+	0022ccc0 R
+	00113420 R
+	0020a980 R
+	0020a968 R
+	00113440 R
+	00113460 R
+00194730 R*/
     std::fstream fh;
     std::string filename;
     cout << "Archivo a leer" << endl;
     cin >> filename;
-    fh.open("C:/Users/Marzo/OneDrive/Documents/Q4 2021/Siso 2/Proyecto/"+filename+".trace", std::fstream::in | std::fstream::binary);
+    fh.open("C:/Users/Skytech/OneDrive/Documents/Q4 2021/Siso 2/Proyecto/"+filename+".trace", std::fstream::in | std::fstream::binary);
     int count{0};
     if (fh.is_open())
     {
+        NodeList* nodeList=new NodeList();
         std::string line;
         while (std::getline(fh, line))
         {
             traceRecord traceLine;
             traceLine = hydrateRecord(traceLine, line);
             cout << "----------------------------------------------------" << endl;
-            cout <<"Record ["<<count<<"]"<<" Mem.Address: [" << traceLine.memAddress << "]"<<"Operation Type: [" << traceLine.operationType<<"]" << endl;
+            cout << "--------------------RECORD["<<count<<"]-----------------------" << endl;
+            cout << "----------------------------------------------------" << endl;
+            Node* newNode = new Node(traceLine);
+            if (nodeList->origin==NULL)
+            {
+                nodeList->origin = newNode;
+                nodeList->lastNode = newNode;
+            }
+            else
+            {
+                nodeList->lastNode->next = newNode;
+                nodeList->lastNode = newNode;
+            }
+            cout <<nodeList->toString(count)<< endl;
             count++;
         }
     }
